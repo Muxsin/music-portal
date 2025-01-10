33e0b37cf7443018618e0919e2631384347368a0
n3sh0w <muxsin.tpu@gmail.com>
2025-01-11 04:14:49 +0700

    Created migrations and models

---
+++ app/Models/Artist.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Artist extends Model
+{
+    use HasFactory;
+
+    public function albums()
+    {
+        return $this->hasMany(Album::class);
+    }
+
+    public function tracks()
+    {
+        return $this->hasMany(Track::class);
+    }
+
+    public function favorites()
+    {
+        return $this->hasMany(Favorite::class);
+    }
+}
